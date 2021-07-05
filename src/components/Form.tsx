cc6af10e7e9f17e58f2287b0e131088670b0fd5f
savio591 <savio591@hotmail.com>
2021-07-04 23:12:00 -0300

    User form: Desktop styling #3

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,14 +1,26 @@
-import { FormHTMLAttributes, ReactNode } from 'react';
+import { FormHTMLAttributes } from 'react';
 import styles from '../styles/Form.module.scss';
 
 interface FormProps extends FormHTMLAttributes<HTMLFormElement> {
-  children: ReactNode;
+  headerTitle: string;
+  headerSubtitle?: string;
 }
 
-export function Form({ children, ...props }: FormProps): JSX.Element {
+export function Form({
+  children,
+  headerTitle,
+  headerSubtitle,
+  ...props
+}: FormProps): JSX.Element {
   return (
-    <form {...props} className={styles.form}>
-      {children}
-    </form>
+    <div className={styles.container}>
+      <div className={styles.headers}>
+        <h1>{headerTitle}</h1>
+        {!!headerSubtitle && <h2>{headerSubtitle}</h2>}
+      </div>
+      <form {...props} className={styles.form}>
+        {children}
+      </form>
+    </div>
   );
 }
