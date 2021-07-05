9249aa2f54c21712cc3e73d117497441c04d0d6c
savio591 <savio591@hotmail.com>
2021-07-05 17:38:24 -0300

    Micro-components - Input: Show password button #16

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,8 +1,43 @@
-import { InputHTMLAttributes } from 'react';
+import { InputHTMLAttributes, useState } from 'react';
+import { FiEye, FiEyeOff } from 'react-icons/fi';
+
 import styles from '../styles/Input.module.scss';
 
-export function Input({
-  ...props
-}: InputHTMLAttributes<HTMLInputElement>): JSX.Element {
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  eyeButtonHidden?: boolean;
+}
+
+export function Input({ ...props }: InputProps): JSX.Element {
+  const [passwordShown, setPasswordShown] = useState(false);
+  const isPasswordButton = props.type === 'password';
+
+  const togglePassword = (): void => {
+    setPasswordShown(!passwordShown);
+  };
+
+  if (isPasswordButton) {
+    return (
+      <div className={styles.container}>
+        <input
+          {...props}
+          type={passwordShown ? 'text' : 'password'}
+          className={styles.input}
+        />
+        {isPasswordButton && (
+          <button
+            type="button"
+            className={styles.button}
+            onClick={togglePassword}
+            role="switch"
+            aria-checked={passwordShown}
+            aria-hidden="true"
+          >
+            {passwordShown ? <FiEye /> : <FiEyeOff />}
+          </button>
+        )}
+      </div>
+    );
+  }
+
   return <input {...props} className={styles.input} />;
 }
